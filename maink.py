b2204958f2d38e665edb9f2cd17bacccaf615b3a
kkdds <eric.kds@hotmail.com>
2016-05-03 23:19:09 +0800

    label ok,text method 1 input ok

--- maink.py
+++ maink.py
@@ -2,67 +2,67 @@ import kivy
 kivy.require('1.8.6') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.uix.switch import Switch
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
-from kivy.uix.slider import Slider
-from kivy.uix.spinner import Spinner
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.vkeyboard import VKeyboard
 from kivy.clock import Clock
-from kivy.graphics import Color, Rectangle,Line
+from kivy.graphics import *
 
 import RPi.GPIO as GPIO
     
 			
 class MyscreenApp(BoxLayout):
-        # This callback will be bound to the LED toggle and Beep button:
-        def press_callback(self,obj):
-                print("Button pressed,")
-                if obj.state == "down":
-                        print ("button on")
-                        obj.text='Running'
-                        self.btx.text='Rrrr'
-                        self.btx.state='down'
-                        with self.tlb.canvas:
-                                Color(1,1,1,1)
-                                Rectangle(pos=self.tlb.pos,size=self.tlb.size)
-                        #GPIO.output(ledPin, GPIO.HIGH)
-                        #VKeyboard()
-                else:
-                        print ("button off")
-                        print (self.btx.pos)
-                        print (self.btx.pos[1])
-                        obj.text='Stopping'
-                        self.btx.text='ppps'
-                        self.btx.state='normal'
-                        self.btx.background_color=[1,0,0,.8]
-                        #self.tlb=Color(1,0,0,.8)
-                        self.tlb.font_size=32
-                        self.tlb.background_color=[1,0,0,.3]
-                        #self.tlb.canvas.clear()
+        
+	# This callback will be bound to the LED toggle and Beep button:
+	def press_callback(self,obj):
+		print("Button pressed,")
+		if obj.state == "down":
+			print ("button on")
+			obj.text='Running'
+			self.lb2.text='Rrrr'
+			self.lb2.bkcolor=[0,1,0,.5]
+			self.lb1.bkcolor=[0,1,0,.5]
+			#GPIO.output(ledPin, GPIO.HIGH)
+		else:
+			print ("button off")
+			obj.text='Stopping'
+			self.lb2.text='ppps'			
+			self.lb2.bkcolor=[1,0,0,.5]
+			self.lb1.bkcolor=[1,0,0,.5]
+			#GPIO.output(ledPin, GPIO.LOW)
+			
+	def press_btn_b1(self,val):
+		print("b1 : ",val.pos[0])
+		#print("enter : ",txt1.text)
+			
+	def press_btn_b2(self,val):
+		print("b2 : ",val.pos[0])
+			
+	def press_btn_b3(self,val):
+		print("b3 : ",val.pos[0])
+		print("t: ",int(self.txt3.text))
+			
+	def on_enter(self,val):
+		print("enter : ",self , val)
+		
 
-
-                        with self.tlb.canvas:
-                                Color(1,1,0,.8)
-                                Rectangle(pos=self.tlb.pos,size=self.tlb.size)
-                        #GPIO.output(ledPin, GPIO.LOW)
-                        
-        def show_selected_value(self,txt):
-                print("spinner : ",txt.text)
-
-                
-
+def up_sta(self):
+        #print("up_sta : ")
+        pass
+        
+		
 class TestApp(App):
 	def build(self):
 		# Start flashing the LED
-		return MyscreenApp()
+		myscr=MyscreenApp()
+		Clock.schedule_interval(up_sta,1/5)
+		return myscr
 		#return Button()
 		#return Button(text='hhh')
 
