5986768f8a0c809df63acece976055e52c4da77d
kkdds <eric.kds@hotmail.com>
2016-07-11 22:59:45 +0800

    a

--- maink.py
+++ maink.py
@@ -18,7 +18,45 @@ import RPi.GPIO as GPIO
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import os,random
+import configparser
 
+kconfig=configparser.ConfigParser()
+kconfig.read('/home/pi/gpmb/'+"set.ini")
+s1=kconfig.get("gpmb","s1")
+s2=kconfig.get("gpmb","s2")
+s3=kconfig.get("gpmb","s3")
+s4=kconfig.get("gpmb","s4")
+s5=kconfig.get("gpmb","s5")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+io_jx1=21
+io_jx2=20
+io_jx3=16
+io_jx4=26
+io_jx5=19
+io_jx6=13
+io_jx7=6
+io_jx8=5
+GPIO.setup(io_jx1, GPIO.OUT)
+GPIO.setup(io_jx2, GPIO.OUT)
+GPIO.setup(io_jx3, GPIO.OUT)
+GPIO.setup(io_jx4, GPIO.OUT)
+GPIO.setup(io_jx5, GPIO.OUT)
+GPIO.setup(io_jx6, GPIO.OUT)
+GPIO.setup(io_jx7, GPIO.OUT)
+GPIO.setup(io_jx8, GPIO.OUT)
+GPIO.output(io_jx1, 1)
+GPIO.output(io_jx2, 1)
+GPIO.output(io_jx3, 1)
+GPIO.output(io_jx4, 1)
+GPIO.output(io_jx5, 1)
+GPIO.output(io_jx6, 1)
+GPIO.output(io_jx7, 1)
+GPIO.output(io_jx8, 1)
+io_gt=io_jx1 #滚筒
+io_sb=io_jx2 #水泵
+io_ld=io_jx3 #漏斗
 
 Builder.load_file('test.kv')
 
@@ -57,56 +95,89 @@ class MyscreenApp(Screen):
     r_sta=False
     watch_dog=1
 
+    def save_set(self):
+        kconfig.set("gpmb","s1",self.time1.text)
+        kconfig.set("gpmb","s2",self.time2.text)
+        kconfig.set("gpmb","s3",self.time3.text)
+        kconfig.set("gpmb","s4",self.time4.text)
+        kconfig.set("gpmb","s5",self.time5.text)
+        kconfig.write(open('/home/pi/gpmb/'+"set.ini","w"))
+        print("saved "+self.time1.text)
+        pass
+
     def on_focus(self):
-        #print("textd")
         sm.current_screen.watch_dog=1
+        pass
+
+    def on_text(self, value):
+        self.save_set()
+        pass
+
+    def press_btn_b1(self,val):
+        print("b1 1: ",val.pos[0])
+        self.watch_dog=1
+        GPIO.output(io_gt, GPIO.LOW)
+        pass
+    def release_btn_b1(self,val):
+        print("b1 0: ",val.pos[0])
+        self.watch_dog=1
+        GPIO.output(io_gt, GPIO.HIGH)
+        pass
+
+    def press_btn_b2(self,val):
+        print("b2 1: ",val.pos[0])
+        self.watch_dog=1
+        GPIO.output(io_sb, GPIO.LOW)
+        pass
+    def release_btn_b2(self,val):
+        print("b2 0: ",val.pos[0])
+        self.watch_dog=1
+        GPIO.output(io_sb, GPIO.HIGH)
+        pass
+
+    def press_btn_b3(self,val):
+        print("b3 1: ",val.pos[0])
+        self.watch_dog=1
+        GPIO.output(io_ld, GPIO.LOW)
+        pass
+    def release_btn_b3(self,val):
+        print("b3 0: ",val.pos[0])
+        self.watch_dog=1
+        GPIO.output(io_ld, GPIO.HIGH)
         pass
 
     def press_tgb(self):
         if self.tgbtn.state == "down" and self.r_sta==False:
             self.tgbtn.text='运行中'
+            GPIO.output(io_jx4, GPIO.LOW)
         else:
-            print ("button off")
+            print ("tgbtn off")
             self.tgbtn.text='已停止'
             self.tgbtn.state = "normal"
             self.txt3.text='1'
 
-    def press_btn_b1(self,val):
-        print("b1 : ",val.pos[0])
-        self.watch_dog=1
-        #GPIO.output(ledPin, GPIO.LOW)
-        pass
-
-    def press_btn_b2(self,val):
-        print("b2 : ",val.pos[0])
-        self.watch_dog=1
-        #GPIO.output(ledPin, GPIO.LOW)
-        pass
-
-    def press_btn_b3(self,val):
-        print("b3 : ",val.pos[0])
-        self.watch_dog=1
-        #GPIO.output(ledPin, GPIO.LOW)
-        pass
-
     def sch_m1(self,dt):            
         print("sch_m1 done: ",datetime.datetime.now())
         Clock.schedule_once(self.sch_m2,int(self.time2.text)/10)
+        GPIO.output(io_jx5, GPIO.LOW)
         pass
 
     def sch_m2(self,dt):
         print("sch_m2 done: ",datetime.datetime.now())
         Clock.schedule_once(self.sch_m3,int(self.time3.text))
+        GPIO.output(io_jx6, GPIO.LOW)
         pass
 
     def sch_m3(self,dt):
         print("sch_m3 done: ",datetime.datetime.now())
         Clock.schedule_once(self.sch_m4,int(self.time4.text)/2)
+        GPIO.output(io_jx7, GPIO.LOW)
         pass
 
     def sch_m4(self,dt):
         print("sch_m4 done: ",datetime.datetime.now())
-        Clock.schedule_once(self.sch_fin,int(self.time4.text)/2)
+        Clock.schedule_once(self.sch_fin,int(self.time5.text)/2)
+        GPIO.output(io_jx8, GPIO.LOW)
         pass
 
     def sch_fin(self,dt):
@@ -123,7 +194,12 @@ class MyscreenApp(Screen):
         self.txt3.disabled=False
         count=int(self.txt3.text)
         count=count-1
-        self.txt3.text=str(count) 
+        self.txt3.text=str(count)
+        GPIO.output(io_jx4, GPIO.HIGH)
+        GPIO.output(io_jx5, GPIO.HIGH)
+        GPIO.output(io_jx6, GPIO.HIGH)
+        GPIO.output(io_jx7, GPIO.HIGH)
+        GPIO.output(io_jx8, GPIO.HIGH)
 
     def update_sta(self,dt):
         self=sm.current_screen
@@ -174,7 +250,7 @@ class MyscreenApp(Screen):
             self.tgbtn.text='已停止'
             
         self.watch_dog+=1
-        if self.watch_dog>60:
+        if self.watch_dog>1600:
             self.watch_dog=1
             #sescr.chpic()
             Clock.schedule_interval(sescr.chpic,5)
@@ -189,6 +265,12 @@ myscr=MyscreenApp(name='menu')
 sm.add_widget(myscr)
 sescr=SettingsScreen(name='settings')
 sm.add_widget(sescr)
+
+myscr.time1.text=s1
+myscr.time2.text=s2
+myscr.time3.text=s3
+myscr.time4.text=s4
+myscr.time5.text=s5
 
 class TestApp(App):
     def build(self):        
